b49556bf4c02d0a17e05a921acb1fea21cb34c43
joseluis <joluferre@hotmail.com>
2020-03-03 13:59:42 +0100

    Lógica Incrementos

---
+++ src/Ejerciciosbasicos/OperadoresIncyDecr1.java
@@ -0,0 +1,24 @@
+package Ejerciciosbasicos;
+
+public class OperadoresIncyDecr1 {
+
+	public static void main(String[] args) {
+		char a = 'c';
+		char b;
+
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(++a);
+		b = --a;
+		System.out.println(++a);
+		a++;
+		--b;
+		System.out.println(b++);
+		System.out.println(++b);
+		System.out.println(++a);
+		System.out.println(b--);
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
