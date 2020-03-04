e0d4abe19f8cfed5b9e3407946e643117af488dc
joseluisferreirogomez <joseluisferreirogomez@192.168.43.3>
2020-03-04 13:51:28 +0100

    canbios

--- src/com/ipartek/formacion/Deporte.java
+++ src/com/ipartek/formacion/Deporte.java
@@ -4,31 +4,20 @@ import java.io.PrintStream;
 
 public class Deporte {
 
-	// ATRIBUTOS
-	// siempre son 'private' para que no se puedan manipular desde fuera de esta
+	public static void main(String[] args) {
+		// ATRIBUTOS
+		// siempre son 'private' para que no se puedan manipular desde fuera de esta
 
-	private String name;
-	private String descripcion;
-	private int numero;
-}
-	
-    {boolean(if (futbol == 0)
-    }
-		{
-			System.out.println("Es V");
-		}
+		private String name;
+		private String descripcion;
+		private int numero;
 
-	else { int baloncesto;
-	if (baloncesto == 0) {
-		System.out.println("Es F");
+		PrintStream teclado;
+		teclado.close(); // cerramos el teclado
 
 	}
 	
-	{
+	
 
-	PrintStream teclado;
-	teclado.close(); // cerramos el teclado
-
-
-	}
+	
 }
